24a54abb6dd201b2e9f0d99b140eb731e5e3ac46
Manuel Araoz <manuelaraoz@gmail.com>
2020-07-31 21:06:32 -0300

    rudimentary scroll execution mechanics

--- scrolls/__init__.py
+++ scrolls/__init__.py
@@ -1,1 +1,5 @@
 from scrolls.version import VERSION as __version__
+
+from scrolls import run
+
+__all__ = ["run"]
---
+++ scrolls/run.py
@@ -0,0 +1,26 @@
+import openai
+import os
+import json
+from collections import namedtuple
+
+
+
+SCROLLS_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def run(name):
+    prefix = SCROLLS_DIR+os.path.sep+name
+    kwargs = {}
+    prompt = open(prefix+'.txt', "r").read()
+    if os.path.isfile(prefix+'.json'):
+        kwargs_file = open(prefix+'.json', "r").read()
+        kwargs = json.loads(kwargs_file)
+
+    response = openai.Completion.create(
+            engine="davinci", prompt=prompt, **kwargs)
+    return response.choices[0].text
+
+
+if __name__ == "__main__":
+    import sys
+    print(run(sys.argv[1]))
